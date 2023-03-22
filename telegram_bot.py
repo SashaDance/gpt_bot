42ee5d62c5971fca03d51876088a82914acfc28b
SashaDance <sksslr@mail.ru>
2023-03-22 14:55:02 +0300

    Async programming added

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,24 +7,24 @@ import asyncio
 
 class TelegramBot:
     def __init__(self, token, openai_api_key):
-        self.bot = telebot.TeleBot(token)
+        self.bot = AsyncTeleBot(token)
         self.openai_api_key = openai_api_key
         self.openai_api = OpenAI_API(self.openai_api_key)
 
         @self.bot.message_handler(commands=['start'])
-        def send_greetings(message):
+        async def send_greetings(message):
             self.markup = types.ReplyKeyboardMarkup()
             info_button = types.KeyboardButton('Info')
             buy_button = types.KeyboardButton('Buy')
             self.markup.row(info_button, buy_button)
-            self.bot.send_message(message.chat.id, "Hi, i am gpt-4 bot!", reply_markup=self.markup)
+            await self.bot.send_message(message.chat.id, "Hi, i am gpt-4 bot!", reply_markup=self.markup)
 
         @self.bot.message_handler(func=lambda message: True)
-        def generate_response(message):
+        async def generate_response(message):
             input_text = message
             response = self.openai_api.generate_text(input_text)
-            self.bot.reply_to(message, response)
+            await self.bot.reply_to(message, response)
 
-    def start_bot(self):
-        self.bot.polling()
+    async def start_bot(self):
+        await self.bot.polling()
 
